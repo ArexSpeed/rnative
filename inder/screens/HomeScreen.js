84be50c5cebe7e1eccc2a4e37e1fc58d1a16f35b
Arek <arexspeed95@gmail.com>
2023-07-06 14:24:42 +0200

    home, login page

--- inder/screens/HomeScreen.js
+++ inder/screens/HomeScreen.js
@@ -1,20 +1,48 @@
-import React from "react";
-import { useNavigation } from "@react-navigation/core";
-import { View, Text, Button } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import useAuth from "../hooks/useAuth";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
+
   return (
-    <View>
+    <SafeAreaView>
+      {/* Header */}
+      <View className="relative flex-row items-center justify-between">
+        <TouchableOpacity onPress={logout}>
+          <Image
+            className="w-10 h-10 rounded-full"
+            source={{ uri: user.photoURL }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+          <Image
+            className="h-14 w-14"
+            source={require("../logo.png")}
+            color="#FF5864"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="chatbubbles-sharp" size={30} />
+        </TouchableOpacity>
+      </View>
       <Text>Home Screen</Text>
       <Button
         title="Go to Chat Screen"
         onPress={() => navigation.navigate("Chat")}
       />
       <Button title="Logout" onPress={logout} />
-    </View>
+    </SafeAreaView>
   );
 };
 
--- inder/screens/LoginScreen.js
+++ inder/screens/LoginScreen.js
@@ -1,15 +1,42 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import useAuth from "../hooks/useAuth";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
   const { user, loading, signInWithGoogle } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   console.log(user);
   return (
-    <View>
-      <Text>{loading ? "loading..." : "Login to the app"}</Text>
-      <Button title="Login" onPress={signInWithGoogle} />
+    <View className="flex-1">
+      <ImageBackground
+        resizeMode="cover"
+        className="flex-1"
+        source={{ uri: "https://tinder.com/static/tinder.png" }}
+      >
+        <TouchableOpacity
+          className="absolute bottom-40 w-52"
+          style={{ marginHorizontal: "25%" }}
+          onPress={signInWithGoogle}
+        >
+          <Text className="font-semibold text-center">
+            Sign in and get swiping
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
